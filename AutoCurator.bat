2e703be247800020a35909502902c56aa9c63009
Zach K <huntingmanzach@gmail.com>
2021-03-15 18:35:32 -0400

    Delete AutoCurator.bat

--- AutoCurator.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM This file purely exists for people who don't have python set up correctly.
-REM You can drag and drop a text file onto this batch file to curate everything in the text file.
-py AutoCurator.py %*
-
-pause
